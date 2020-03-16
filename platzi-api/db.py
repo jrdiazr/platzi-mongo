8cc5588e9ead07b11e01b3af4c4e8d224cb748ba
Jose Diaz <diazrodriguez.jr@gmail.com>
2020-03-15 19:28:58 -0500

    API Mongo terminada

--- platzi-api/db.py
+++ platzi-api/db.py
@@ -4,7 +4,8 @@ from pymongo import MongoClient, DESCENDING
 from werkzeug.local import LocalProxy
 
 
-# Este método se encarga de configurar la conexión con la base de datos
+
+# Este metodo se encarga de configurar la conexion con la base de datos
 def get_db():
     platzi_db = current_app.config['PLATZI_DB_URI']
     client = MongoClient(platzi_db)
@@ -26,20 +27,19 @@ def collection_stats(collection_nombre):
 
 
 def crear_carrera(json):
-    return str('Falta por implementar')
+    return str(db.carreras.insert_one(json).inserted_id)
 
 
 def consultar_carrera_por_id(carrera_id):
-    return str('Falta por implementar')
+    return dumps(db.carreras.find_one({'_id': ObjectId(carrera_id)}))
 
 
 def actualizar_carrera(carrera):
-    # Esta funcion solamente actualiza nombre y descripcion de la carrera
-    return str('Falta por implementar')
+    return str(db.carreras.update_one({'_id': ObjectId(carrera['_id'])}, {'$set': {'nombre':carrera['nombre'], 'descripcion':carrera['descripcion']}}).modified_count)
 
 
 def borrar_carrera_por_id(carrera_id):
-    return str('Falta por implementar')
+    return str(db.carreras.delete_one({'_id': ObjectId(carrera_id)}))
 
 
 # Clase de operadores
@@ -48,36 +48,37 @@ def consultar_carreras(skip, limit):
 
 
 def agregar_curso(json):
-    return str('Falta por implementar')
+    curso = consultar_curso_por_id_proyeccion(json['id_curso'], proyeccion={'nombre': 1})
+    return str(db.carreras.update_one({'_id':ObjectId(json['id_carrera'])},{'$addToSet': {'cursos': curso}}).modified_count)
 
 
 def borrar_curso_de_carrera(json):
-    return str('Falta por implementar')
+    return str(db.carreras.update_one({'_id':ObjectId(json['id_carrera'])}, {'$pull': {'cursos': {'_id':ObjectId(json['id_curso'])}}}).modified_count)
 
 # -----------------Cursos-------------------------
 
 
 def crear_curso(json):
-    return str('Falta por implementar')
+    return str(db.cursos.insert_one(json).inserted_id)
 
 
 def consultar_curso_por_id(id_curso):
-    return str('Falta por implementar')
+    return dumps(db.cursos.find_one({'_id': ObjectId(id_curso)}))
 
 
 def actualizar_curso(curso):
     # Esta funcion solamente actualiza nombre, descripcion y clases del curso
-    return str('Falta por implementar')
+    return str(db.cursos.update_one({'_id': ObjectId(curso['_id'])}, {'$set': {'nombre':curso['nombre'], 'descripcion':curso['descripcion'], 'clases':curso['clases']}}).modified_count)
 
 
 def borrar_curso_por_id(curso_id):
-    return str('Falta por implementar')
+    return str(db.cursos.delete_one({'_id': ObjectId(curso_id)}))
 
 
 def consultar_curso_por_id_proyeccion(id_curso, proyeccion=None):
-    return str('Falta por implementar')
+    return str(db.cursos.find_one({'id_': ObjectId(id_curso)}, proyeccion))
 
 
 def consultar_curso_por_nombre(nombre):
-    return str('Falta por implementar')
+    return dumps(db.cursos.find({'$text': {'$search': nombre}}))
 
